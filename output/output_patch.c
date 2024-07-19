bd7ae8c2cff9cb107d54930a2157a86ef547d322
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2024-07-19 11:22:58 -0400

    Update output_patch.c

--- output/output_patch.c
+++ output/output_patch.c
@@ -172,9 +172,9 @@ void	output_patch(
                     top30cm_potential_sat * 1000.0,
                     top60cm_storage * 1000.0,
                     top60cm_potential_sat * 1000.0,
-			patch[0].irrigation_sat,
-			patch[0].irr_demand = 0.0, 
-			patch[0].available_soilwater = 0.0
+			patch[0].irrigation_sat * 1000.0,
+			patch[0].irr_demand * 1000.0, 
+			patch[0].available_soilwater * 1000.0
                     );
 
 	if (check <= 0) {
